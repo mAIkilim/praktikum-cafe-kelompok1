3627337550dacba99402cc9c7c7e5db97cf3c378
Yoowel <lanangyoel@gmail.com>
2026-09-23 15:18:26 +0700

    Fix tombol tambah ke keranjang

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Menyimpan jumlah item yang sudah ditambahkan ke keranjang
 let cartCount = 0;
 
-function tambahKeranjang(namaMenu) {
+function addToKeranjang(namaMenu) {
   cartCount++;
   document.getElementById('cart-count').innerText = cartCount;
   console.log(namaMenu + ' ditambahkan ke keranjang');
